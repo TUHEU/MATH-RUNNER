a8e47dd4e42b329daf4f77892107031e9558221b
DalingtonChris <andydarlington3@gmail.com>
2025-08-14 16:30:20 +0100

    update test andy.py

--- test andy.py
+++ test andy.py
@@ -1,1 +1,143 @@
-print("heloo")
+import pygame
+import random
+from sys import exit
+
+pygame.init()
+window=pygame.display.Info()
+x=window.current_w
+y=window.current_h
+screen=pygame.display.set_mode((x,y))
+clock=pygame.time.Clock() 
+unitx=x/1000
+unity=y/1000
+
+#pages booleans
+menu_scrn=True
+#font
+font1=pygame.font.Font("Assets/Fonts/1.TTF",50)
+
+#animation variables
+signs=['+','-','/','*']
+
+
+#player
+class character:
+    def __init__(self,size,path,pos_player):
+        self.size=size
+        self.path=path
+        self.frame=pygame.image.load(path).convert_alpha()
+        self.frame=pygame.transform.scale(self.frame,(self.frame.get_width()*size[0],self.frame.get_height()*aqaaasize[1]))
+        self.pos_player=pos_player
+        self.rect=self.frame.get_rect(bottomleft=(0,0))
+
+    # def convertframe(self):
+    #     pygame
+        
+    def createanimation(self):
+        pass
+
+#equation                       
+class equationC:
+    def __init__(self,eqaution="",sign="",ans=0,isactive=False,delay=0):
+        self.equation=eqaution
+        self.isactive=isactive
+        self.sign=sign
+        self.ans=ans
+        self.delay=delay
+    def generate_equation(self,signs):
+        self.equation=str(random.randint(1,50))+signs[random.randint(0,3)]+str(random.randint(1,50))
+        self.ans=str(int(eval(self.equation)))
+        self.delay=0
+        self.equation=self.equation+"="+self.ans
+        return self.equation
+    def active(self,wait,dt,cur_equation,eqn_locx,eqn_locy):
+        if(self.delay<wait):
+                    equationText=font1.render(f"{cur_equation[i]}",True,"Black")
+                    #equationText=pygame.transform.rotate(equationText,random.randint(0,45))
+                    screen.blit(equationText,(eqn_locx[i],eqn_locy[i]))
+                    self.delay+=dt
+                    self.isactive=True
+        else:
+            cur_equation[i]=(equ.generate_equation(signs))
+            eqn_locx[i]=(random.randint(int(unitx*100),int(x-(unitx*200))))
+            eqn_locy[i]=(random.randint(int(unity*100),int(y-(unity*150))))
+            self.isactive=False
+        return self.isactive
+
+
+#button class
+class button:
+    def __init__(self,default_path,size,rect_pos,key=""):
+        self.size=size
+        self.default_img=pygame.image.load(default_path).convert_alpha()
+        self.default_img=pygame.transform.scale(self.default_img,self.size)
+        self.touched_img=pygame.transform.scale(self.default_img,(self.size[0]*.8,self.size[1]*.8))
+        self.rect=self.default_img.get_rect(topleft=rect_pos)
+        self.touched=False
+        self.touching=True
+        self.key=key
+    def draw(self,screen):
+        if self.touched: img=self.touched_img 
+        else:img=self.default_img
+        screen.blit(img, self.rect)
+    def handle_event(self, event, mouse_pos):
+        self.touched = self.rect.collidepoint(mouse_pos)
+        if self.touched and self.touching:
+            touch_sound.play()
+        self.touching=False
+        if(not self.touched):self.touching=True
+        if self.touched and event.type == pygame.MOUSEBUTTONDOWN: 
+            return self.key    
+        return None
+#button list
+buttons=[button("Assets\Buttons\Default\start.png",(x/4,y/8),((x/2)-(unitx*120),(y/2)-(unity*300)),"start"),
+         button("Assets\Buttons\Default\options.png",(x/4,y/8),((x/2)-(unitx*120),(y/2)-(unity*150)),"options"),
+         button("Assets\Buttons\Default\custom level.png",(x/4,y/8),((x/2)-(unitx*120),(y/2)-(unity)),"custom level"),
+         button("Assets\Buttons\Default\exit.png",(x/4,y/8),((x/2)-(unitx*120),(y/2)+(unity*150)),"exit")]
+
+#equations list
+equations=[equationC(),equationC(),equationC(),equationC(),equationC(),equationC(),equationC(),equationC(),equationC(),equationC(),equationC(),equationC(),equationC()]
+#sounds
+pygame.mixer.init()
+# pygame.mixer.music.load('Assets\Sounds\touch.mp3')
+# pygame.mixer.music.play()
+# pygame.mixer.music.set_volume(0.25)
+
+touch_sound=pygame.mixer.Sound("Assets/Sounds/touch.mp3")
+
+#menu
+menu=pygame.image.load("Assets/Menu/menu.jpg")
+menu_rect=menu.get_rect(topleft=(0,0))
+menu=pygame.transform.scale(menu,(x,y))
+j=0
+cur_equation=["","","","","","","","","","","","","",]
+eqn_locx=[0,0,0,0,0,0,0,0,0,0,0,0,0]
+eqn_locy=[0,0,0,0,0,0,0,0,0,0,0,0,0]
+while(True):
+    dt=clock.tick(60)
+    mouse = pygame.mouse.get_pos() 
+    kpressed=pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT or kpressed[pygame.K_ESCAPE]:
+            pygame.quit()
+            exit()
+    screen.blit(menu,menu_rect)
+    if menu:
+        i=0
+        for equ in equations:
+            print(equ.isactive)
+            wait=(random.randint(400,800))
+            if not equ.active(wait,dt,cur_equation,eqn_locx,eqn_locy):
+                if(j<13):
+                    cur_equation[i]=(equ.generate_equation(signs))
+                    eqn_locx[i]=(random.randint(int(unitx*100),int(x-(unitx*100))))
+                    eqn_locy[i]=(random.randint(int(unity*100),int(y-(unity*100))))
+            i+=1
+            j+=1
+        for button in buttons:
+            button.draw(screen)
+            button.handle_event(event,mouse)
+            if(button.handle_event(event,mouse)=="exit"):
+                pygame.quit()
+                exit()
+    pygame.display.update()
