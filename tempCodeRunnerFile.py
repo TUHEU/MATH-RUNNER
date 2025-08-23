08e6fb38b50da3d8e465321fa64a475a87df2906
Kynmmarshall <kynmmarshall@gmail.com>
2025-08-23 21:08:42 +0100

    updated collision detection for player and enemies

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-if  not self.front:
-        #     self.enemysuf = enemy1_Walk[int(self.index)].frameF
-        #     self.enemyrect = self.enemysuf.get_rect(bottomleft=rect)
-        #     self.enemyrect.left -= 3 * unitx
-        # elif self.front:
-        #     self.front = False
-        #     self.enemysuf= enemy1_Walk[int(self.index)].frameB
-        #     self.enemyrect = self.enemysuf.get_rect(bottomleft=rect)
-        #     self.enemyrect.left += 3 * unitx
